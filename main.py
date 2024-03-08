7c86549115b86175c83ee9243fce30b59214ceee
Giohan Melo <giohandmelo@outlook.com>
2024-03-08 00:30:47 -0500

    Mandar correo

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #Instale el pip pywin32
+
+#Prueba de como mandar un correo en Outlook
+import win32com.client as client
+outlook = client.Dispatch("Outlook.Application")
+message = outlook.CreateItem(0)
+message.Display()
+message.To = "giohandmelo@outlook.com"
+message.CC = "giohandmelo@outlook.com"
+message.BCC = "giohandmelo@outlook.com"
+message.Subject = "Happy Birthday"
+message.Body = "Wish you a happy birthday!"
+message.Save()
+message.Send()
